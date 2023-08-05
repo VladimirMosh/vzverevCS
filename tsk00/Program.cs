5aea8adc4986e4a773515257fd2c7ae953da44b8
Zverev <you@example.com>
2023-08-05 20:59:06 +0300

    Napisal zadachy4

--- tsk00/Program.cs
+++ tsk00/Program.cs
@@ -1,9 +1,4 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
-
-
+﻿
 Console.WriteLine ("Веди число"); 
 int number = Convert.ToInt32(Console.ReadLine());
 int square = number * number;
--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.WriteLine("Первое число")
+int Num1 = convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число")
+int Num2 = convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Третье число")
+int Num3 = convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (Num1 > max)
+{
+    max = Num1;
+}
+if (Num2 > max)
+{
+    max = Num2;
+}
+if (Num3 > max)
+{
+    max = Num3;
+}
+
+Console.WriteLine("максимальное число" max);
