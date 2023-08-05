9cd101cb50da579be5d44c927e74ef24a71b452b
Zverev <you@example.com>
2023-08-05 21:08:09 +0300

    Napisal zadachy4

--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -4,13 +4,17 @@
 // 22 3 9 -> 22
 
 
-Console.WriteLine("Первое число")
-int Num1 = convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Второе число")
-int Num2 = convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Третье число")
-int Num3 = convert.ToInt32(Console.ReadLine());
+Console.Write("Первое число  ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Второе число  ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Третье число  ");
+int Num3 = Convert.ToInt32(Console.ReadLine());
+
 int max = 0;
+
 if (Num1 > max)
 {
     max = Num1;
@@ -24,4 +28,4 @@ if (Num3 > max)
     max = Num3;
 }
 
-Console.WriteLine("максимальное число" max);
+Console.WriteLine("максимальное число " + max);
