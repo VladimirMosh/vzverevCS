211c2a4962fe64fff4cf0175f055ca9cc820d37f
Zverev <you@example.com>
2023-08-10 21:54:12 +0300

    dobavil zadachy10

--- zadacha10/Program.cs
+++ zadacha10/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+
+
+Console.Write("Введи трёхзначное число- ");
+int threeDigitNum = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNum);
+Console.WriteLine("вторая цифра этого числа -> " + stringNumber[1]);
